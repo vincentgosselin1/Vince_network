1907a8d7f3321cf3c9cc541f80430e4eced6ad84
vincentgosselin1 <vincentgosselin1@gmail.com>
2016-10-05 22:31:47 -0400

    link to signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
 root 'welcome#index'
+match '/signup', to: 'users#new', via: 'get'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
