e369e8054c0ca6de780a0f8f1eb5fc8fe12b1e26
vincentgosselin1 <vincentgosselin1@gmail.com>
2016-10-06 22:23:19 -0400

    first user created using form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,22 @@ class UsersController < ApplicationController
   end
   
   def new
+  	@user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "You are now apart of Vince's little social network!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+      params.require(:user).permit(:name, :email)
   end
 end
